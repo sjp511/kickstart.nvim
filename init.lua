05ce7f28a2928606a830856c99c8586efaa2f23e
SJ Paik <seungjae.paik@joinklover.com>
2025-04-29 14:18:03 -0700

    auto format

--- init.lua
+++ init.lua
@@ -790,7 +790,7 @@ require('lazy').setup({
           return nil
         else
           return {
-            timeout_ms = 500,
+            timeout_ms = 1000,
             lsp_format = 'fallback',
           }
         end
@@ -798,7 +798,8 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        rust = { 'rustfmt', lsp_format = 'fallback' },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
